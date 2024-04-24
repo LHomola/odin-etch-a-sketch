339462e3b16b3bf4061e8f6bb64786b3a909dcb8
LHomola <lhomola83@gmail.com>
2024-04-24 22:36:22 +0100

    create loop in app.js to populate main-container with a grid of divs

---
+++ app.js
@@ -0,0 +1,21 @@
+const gridSize = 16; // number of divs in a row / column
+
+function createDivGrid() {
+    const mainContainer = document.querySelector(".main-container");
+    const mainContainerWidth = mainContainer.offsetWidth / gridSize;
+
+    // populate mainContainer with a grid of divs so that these can be used to draw on with mouse
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const divCell = document.createElement("div");
+    
+        divCell.style.height = `${mainContainerWidth}px`;
+        divCell.style.width = `${mainContainerWidth}px`;
+
+        const divContent = document.createTextNode(`${i}`);
+        
+        divCell.appendChild(divContent);
+        mainContainer.appendChild(divCell);
+    }
+}
+
+createDivGrid();
