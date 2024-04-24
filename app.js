974397b1d93b06e4f8e6f1d7c79c9d096c71d3fc
LHomola <lhomola83@gmail.com>
2024-04-24 23:32:57 +0100

    add code to get grid size from user and generate new grid

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-const gridSize = 16; // number of divs in a row / column
+const MAX_SIZE = 100;
+let gridSize = 16; // number of divs in a row / column
+
+function changeGridSize() {    
+    const mainContainer = document.querySelector(".main-container");
+
+    // limit size of grid to avoid potential delays, freezing, or crashing
+    do {
+        gridSize = prompt("Provide grid size in squares / row: ");
+    } while (gridSize > MAX_SIZE);
+    
+    mainContainer.innerHTML = "";
+    createDivGrid();
+    updateSizeBtnlabel();
+}
+
+function attachGridSizeButtonEventListener() {
+    const sizeBtn = document.querySelector("#gridSizeBtn");
+    sizeBtn.addEventListener("click", () => { changeGridSize() });    
+    updateSizeBtnlabel();
+}
 
 function createDivGrid() {
     const mainContainer = document.querySelector(".main-container");
-    const mainContainerWidth = mainContainer.offsetWidth / gridSize;
+    const mainContainerWidth = mainContainer.offsetWidth / gridSize; // add borders to div dimensions
 
     // populate mainContainer with a grid of divs so that these can be used to draw on with mouse
     for (let i = 0; i < gridSize * gridSize; i++) {
@@ -10,6 +30,7 @@ function createDivGrid() {
     
         divCell.style.backgroundColor = "white";
         divCell.style.border = "1px solid black";
+        divCell.style.boxSizing = "border-box";
         divCell.style.height = `${mainContainerWidth}px`;
         divCell.style.width = `${mainContainerWidth}px`;
 
@@ -23,4 +44,10 @@ function attachHoverEventListener(divCell) {
     divCell.addEventListener("mouseover", () => { divCell.style.backgroundColor = "black"});
 }
 
+function updateSizeBtnlabel() {
+    const sizeBtn = document.querySelector("#gridSizeBtn");
+    sizeBtn.textContent = `Change grid size. Current size: ${gridSize} squares / row`;
+}
+
 createDivGrid();
+attachGridSizeButtonEventListener();
